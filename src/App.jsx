02546183cd84d70f4f53979a1e816a7b8eb7c288
MohammadAmin-1389 <mohammad1389amir@gmail.com>
2026-07-23 16:47:32 +0330

    start proje

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="text-[100px] flex items-center justify-center font-bold ">
+      <h1>
+        Start proje
+      </h1>
+      
+      
+    </div>
+  )
+}
+
+export default App
